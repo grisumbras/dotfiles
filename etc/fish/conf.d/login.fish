3201fa72d83d00d15b8e69facfa365a244f1e8cb
Архипов Дмитрий <grisumbras@gmail.com>
2018-12-04 22:42:25 +0300

    moved gnupg stuff to XDG dir

--- etc/fish/conf.d/login.fish
+++ etc/fish/conf.d/login.fish
@@ -1,12 +1,14 @@
 set -gx PATH "$USER_ROOT/bin" \
-		/usr/local/sbin \
-		/usr/local/bin \
-		/usr/sbin \
-		/usr/bin \
-		/sbin \
-		/bin \
-		/usr/local/games \
-		/usr/games \
-		/snap/bin
+  /usr/local/sbin \
+  /usr/local/bin \
+  /usr/sbin \
+  /usr/bin \
+  /sbin \
+  /bin \
+  /usr/local/games \
+  /usr/games \
+  /snap/bin
 
 set -gx LD_LIBRARY_PATH "$USER_ROOT/lib"
+
+set -gx GNUPGHOME $XDG_CONFIG_HOME/gnupg
