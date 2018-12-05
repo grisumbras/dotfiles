30346106f21df39110e088606f640e0c2675a469
Архипов Дмитрий <grisumbras@gmail.com>
2018-12-05 21:15:18 +0300

    updated shell configs

--- etc/fish/conf.d/rc.fish
+++ etc/fish/conf.d/rc.fish
@@ -1,6 +1,3 @@
-set -gx EDITOR vim
-set -gx MANPAGER "view -c 'set ft=man' -"
-
 set -gx BOOST_BUILD_PATH $XDG_CONFIG_HOME/boost-build
 
 set -gx COOKIECUTTER_CONFIG $XDG_CONFIG_HOME/cookiecutter/config.yaml
@@ -29,57 +26,60 @@ set -gx PROMPT_VIEWS prompt-badges-view                                       \
 set -gx PROMPT_RVIEWS prompt-scm-view
 
 if test x$TERM = xlinux
-    set -g PROMPT_LLMARG ''
-    set -g PROMPT_LSEP1 ''
-    set -g PROMPT_LSEP2 ''
-    set -g PROMPT_LRMARG ' '
-    set -g PROMPT_RLMARG ' '
-    set -g PROMPT_RSEP1 ''
-    set -g PROMPT_RSEP2 ''
-    set -g PROMPT_RRMARG ''
+  set -g PROMPT_LLMARG ''
+  set -g PROMPT_LSEP1 ''
+  set -g PROMPT_LSEP2 ''
+  set -g PROMPT_LRMARG ' '
+  set -g PROMPT_RLMARG ' '
+  set -g PROMPT_RSEP1 ''
+  set -g PROMPT_RSEP2 ''
+  set -g PROMPT_RRMARG ''
 
-    set -g PROMPT_MARK_PRIVILEGED '!'
-    set -g PROMPT_MARK_JOBS %
-    set -g PROMPT_MARK_FAILED X
-    set -g PROMPT_MARK_HOME '~'
+  set -g PROMPT_MARK_PRIVILEGED '!'
+  set -g PROMPT_MARK_JOBS %
+  set -g PROMPT_MARK_FAILED X
+  set -g PROMPT_MARK_HOME '~'
 
-    set -g PROMPT_MARK_SCM_REPO @
-    set -g PROMPT_MARK_SCM_STAGED +
-    set -g PROMPT_MARK_SCM_UNSTAGED -
-    set -g PROMPT_MARK_SCM_UNTRACKED .
-    set -g PROMPT_MARK_SCM_DETACHED ~
-    set -g PROMPT_MARK_SCM_BRANCH B
-    set -g PROMPT_MARK_SCM_REMOTE_BRANCH R
-    set -g PROMPT_MARK_SCM_TAG T
-    set -g PROMPT_MARK_SCM_AHEAD A
-    set -g PROMPT_MARK_SCM_BEHIND V
-    set -g PROMPT_MARK_SCM_DIVERGED X
-    set -g PROMPT_MARK_SCM_STASH _
+  set -g PROMPT_MARK_SCM_REPO @
+  set -g PROMPT_MARK_SCM_STAGED +
+  set -g PROMPT_MARK_SCM_UNSTAGED -
+  set -g PROMPT_MARK_SCM_UNTRACKED .
+  set -g PROMPT_MARK_SCM_DETACHED ~
+  set -g PROMPT_MARK_SCM_BRANCH B
+  set -g PROMPT_MARK_SCM_REMOTE_BRANCH R
+  set -g PROMPT_MARK_SCM_TAG T
+  set -g PROMPT_MARK_SCM_AHEAD A
+  set -g PROMPT_MARK_SCM_BEHIND V
+  set -g PROMPT_MARK_SCM_DIVERGED X
+  set -g PROMPT_MARK_SCM_STASH _
 else
-    set -g PROMPT_LLMARG ' '
-    set -g PROMPT_LSEP1 \uE0B0
-    set -g PROMPT_LSEP2 \uE0B1
-    set -g PROMPT_LRMARG ' '
-    set -gx PROMPT_RLMARG ' '
-    set -gx PROMPT_RSEP1 \uE0B2
-    set -gx PROMPT_RSEP2 \uE0B3
-    set -gx PROMPT_RRMARG ' '
+  set -gx EDITOR vim
+  set -gx MANPAGER "view -c 'set ft=man' -"
 
-    set -g PROMPT_MARK_PRIVILEGED ⚡
-    set -g PROMPT_MARK_JOBS \uF0AD
-    set -g PROMPT_MARK_FAILED \uF00D
-    set -g PROMPT_MARK_HOME \uF015
+  set -g PROMPT_LLMARG ' '
+  set -g PROMPT_LSEP1 \uE0B0
+  set -g PROMPT_LSEP2 \uE0B1
+  set -g PROMPT_LRMARG ' '
+  set -gx PROMPT_RLMARG ' '
+  set -gx PROMPT_RSEP1 \uE0B2
+  set -gx PROMPT_RSEP2 \uE0B3
+  set -gx PROMPT_RRMARG ' '
 
-    set -g PROMPT_MARK_SCM_REPO \uF02D
-    set -g PROMPT_MARK_SCM_STAGED +
-    set -g PROMPT_MARK_SCM_UNSTAGED \u2981
-    set -g PROMPT_MARK_SCM_UNTRACKED \?
-    set -g PROMPT_MARK_SCM_DETACHED \uF074
-    set -g PROMPT_MARK_SCM_BRANCH \uF020
-    set -g PROMPT_MARK_SCM_REMOTE_BRANCH \uF093
-    set -g PROMPT_MARK_SCM_TAG \uF02B
-    set -g PROMPT_MARK_SCM_AHEAD ↑
-    set -g PROMPT_MARK_SCM_BEHIND ↓
-    set -g PROMPT_MARK_SCM_DIVERGED ⇅
-    set -g PROMPT_MARK_SCM_STASH \uF0CF
+  set -g PROMPT_MARK_PRIVILEGED ⚡
+  set -g PROMPT_MARK_JOBS \uF0AD
+  set -g PROMPT_MARK_FAILED \uF00D
+  set -g PROMPT_MARK_HOME \uF015
+
+  set -g PROMPT_MARK_SCM_REPO \uF02D
+  set -g PROMPT_MARK_SCM_STAGED +
+  set -g PROMPT_MARK_SCM_UNSTAGED \u2981
+  set -g PROMPT_MARK_SCM_UNTRACKED \?
+  set -g PROMPT_MARK_SCM_DETACHED \uF074
+  set -g PROMPT_MARK_SCM_BRANCH \uF020
+  set -g PROMPT_MARK_SCM_REMOTE_BRANCH \uF093
+  set -g PROMPT_MARK_SCM_TAG \uF02B
+  set -g PROMPT_MARK_SCM_AHEAD ↑
+  set -g PROMPT_MARK_SCM_BEHIND ↓
+  set -g PROMPT_MARK_SCM_DIVERGED ⇅
+  set -g PROMPT_MARK_SCM_STASH \uF0CF
 end
--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,6 +1,7 @@
 set -gx USER_ROOT $HOME/.local
 set -gx XDG_CONFIG_HOME $USER_ROOT/etc
 set -gx XDG_DATA_HOME $USER_ROOT/share
+set -gx XDG_CACHE_HOME $USER_ROOT/var/tmp
 
 set -l here (dirname (status --current-filename))
 
